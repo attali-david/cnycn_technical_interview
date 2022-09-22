a91f59d9d025fe890f37ab4b6406720ed887d098
attali-david <dattali93@gmail.com>
2022-09-21 19:14:22 -0500

    api calls for lat and lon and weather

--- components/header.tsx
+++ components/header.tsx
@@ -2,14 +2,55 @@ import { useRouter } from "next/router";
 import React, { useState } from "react";
 import { FaMoon, FaSun } from "react-icons/fa";
 
-function Header({ toggler, color }) {
-  const router = useRouter();
-  const [toggle, setToggle] = useState(false);
+function Header({
+  toggler,
+  color,
+  setSearch,
+  submit,
+  setSubmit,
+  setCities,
+  getCities,
+  search,
+}) {
+  function changeHandler(e) {
+    e.preventDefault();
+    setSearch({ city: e.target.value });
+  }
+
+  function submitHandler(e) {
+    e.preventDefault();
+    getCities(search.city);
+    setSubmit(!submit);
+  }
 
   return (
-    <header className="p-4 relative bg-white-50 dark:bg-gray-800 ">
-      <div className="container flex justify-end h-16 mx-auto">
-        <button className="flex content-end" onClick={toggler}>
+    <header className="p-4 relative bg-white dark:bg-gray-800">
+      <div className="container flex justify-between h-16 mx-auto">
+        <h1 className="flex ">Weather App by David Attali</h1>
+        <form className="flex" onSubmit={submitHandler}>
+          <input
+            type="text"
+            placeholder="Search by city"
+            className="bg-gray-800 w-32 py-2 pl-10 text-sm rounded-md sm:w-auto focus:outline-none dark:bg-gray-800 dark:text-gray-100 focus:dark:bg-gray-900 focus:dark:border-violet-400"
+            onChange={changeHandler}
+          />
+          <select>
+            <option>F</option>
+            <option>C</option>
+          </select>
+        </form>
+        <div>
+          {submit && (
+            <select>
+              {setCities.map((result) => (
+                <option key={result.lat}>
+                  {`${result.name}, ${result.state}`}
+                </option>
+              ))}
+            </select>
+          )}
+        </div>
+        <button className="flex " onClick={toggler}>
           {color == "light" ? <FaMoon /> : <FaSun />}
         </button>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,36 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className="bg-white text-black dark:bg-gray-800 dark:text-gray-100">
-      <h1>Hello</h1>
+      <div className="column1">
+        <span>Temp</span>
+        <div className="row1">
+          <h2 id="temp"></h2>
+        </div>
+        <span>Feels Like</span>
+        <div className="row2">
+          <h2 id="feels_like"></h2>
+        </div>
+      </div>
+      <div className="column2">
+        <span>Temp min</span>
+        <div className="row1">
+          <h2 id="temp_min"></h2>
+        </div>
+        <span>Temp max</span>
+        <div className="row2">
+          <h2 id="temp_max"></h2>
+        </div>
+      </div>
+      <div className="column3">
+        <span>Pressure</span>
+        <div className="row1">
+          <h2 id="pressure"></h2>
+        </div>
+        <span>Humidity</span>
+        <div className="row2">
+          <h2 id="humidity"></h2>
+        </div>
+      </div>
     </div>
   );
 };
