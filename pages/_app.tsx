26200e68424216291383b1570ffcbaa576ba6235
attali-david <dattali93@gmail.com>
2022-09-28 16:23:54 -0500

    fixed dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,11 @@ import type { AppProps } from "next/app";
 import { useState, useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="dark:bg-gray-800">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
